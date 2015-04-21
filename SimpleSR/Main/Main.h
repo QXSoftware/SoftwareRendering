35443cba76e8caef581ba3c5d5534505f2e11ef4
xiaocheng <lh563566994@126.com>
2015-04-21 17:21:27 +0800

    创建软渲染项目
    Signed-off-by: xiaocheng <lh563566994@126.com>

---
+++ SimpleSR/Main/Main.h
@@ -0,0 +1,14 @@
+#ifndef Main_h__
+#define Main_h__
+
+#define WIN32_LEAN_AND_MEAN
+
+#include "targetver.h"
+#include <windows.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include "Resource.h"
+
+#endif // Main_h__
