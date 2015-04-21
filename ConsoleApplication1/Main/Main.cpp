7ffaf01752f595d1aac96e4b1f4934e6cc9486b6
xiaocheng <lh563566994@126.com>
2015-04-21 14:41:55 +0800

    Init commit
    Signed-off-by: xiaocheng <lh563566994@126.com>

---
+++ ConsoleApplication1/Main/Main.cpp
@@ -0,0 +1,6 @@
+#include "Main.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
---
+++ ConsoleApplication1/Main/Main.h
@@ -0,0 +1,7 @@
+#ifndef Main_h__
+#define Main_h__
+
+#include <stdio.h>
+#include <tchar.h>
+
+#endif // Main_h__
