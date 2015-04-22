2b859c7a39cf5cecc2bd3ce85d8b88fbebf5d161
xiaocheng <lh563566994@126.com>
2015-04-22 20:19:02 +0800

    窗口居中显示，默认尺寸 800*600
    Signed-off-by: xiaocheng <lh563566994@126.com>

--- SimpleSR/Main/Main.cpp
+++ SimpleSR/Main/Main.cpp
@@ -2,8 +2,6 @@
 
 #define MAX_LOADSTRING 100
 
-// 全局变量: 
-HINSTANCE hInst;								// 当前实例
 TCHAR szTitle[MAX_LOADSTRING];					// 标题栏文本
 TCHAR szWindowClass[MAX_LOADSTRING];			// 主窗口类名
 
@@ -20,16 +18,13 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
-	// TODO:  在此放置代码。
 	MSG msg;
 	HACCEL hAccelTable;
 
-	// 初始化全局字符串
 	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
 	LoadString(hInstance, IDC_SIMPLESR, szWindowClass, MAX_LOADSTRING);
 	MyRegisterClass(hInstance);
 
-	// 执行应用程序初始化: 
 	if (!InitInstance(hInstance, nCmdShow))
 	{
 		return FALSE;
@@ -50,11 +45,6 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	return (int)msg.wParam;
 }
 
-//
-//  函数:  MyRegisterClass()
-//
-//  目的:  注册窗口类。
-//
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
 	WNDCLASSEX wcex;
@@ -76,24 +66,15 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 	return RegisterClassEx(&wcex);
 }
 
-//
-//   函数:  InitInstance(HINSTANCE, int)
-//
-//   目的:  保存实例句柄并创建主窗口
-//
-//   注释: 
-//
-//        在此函数中，我们在全局变量中保存实例句柄并
-//        创建和显示主程序窗口。
-//
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-	HWND hWnd;
+	auto winWidth = 800;
+	auto winHeight = 600;
+	auto scrWidth = GetSystemMetrics(SM_CXSCREEN);
+	auto scrHeight = GetSystemMetrics(SM_CYSCREEN);
 
-	hInst = hInstance; // 将实例句柄存储在全局变量中
-
-	hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+	HWND hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		(scrWidth - winWidth) / 2, (scrHeight - winHeight) / 2, winWidth, winHeight, NULL, NULL, hInstance, NULL);
 
 	if (!hWnd)
 	{
@@ -106,16 +87,6 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 	return TRUE;
 }
 
-//
-//  函数:  WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  目的:    处理主窗口的消息。
-//
-//  WM_COMMAND	- 处理应用程序菜单
-//  WM_PAINT	- 绘制主窗口
-//  WM_DESTROY	- 发送退出消息并返回
-//
-//
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	int wmId, wmEvent;
@@ -124,29 +95,29 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 	switch (message)
 	{
-	case WM_COMMAND:
-		wmId = LOWORD(wParam);
-		wmEvent = HIWORD(wParam);
-		// 分析菜单选择: 
-		switch (wmId)
-		{
-		case IDM_EXIT:
-			DestroyWindow(hWnd);
+		case WM_COMMAND:
+			wmId = LOWORD(wParam);
+			wmEvent = HIWORD(wParam);
+			// 分析菜单选择:
+			switch (wmId)
+			{
+			case IDM_EXIT:
+				DestroyWindow(hWnd);
+				break;
+			default:
+				return DefWindowProc(hWnd, message, wParam, lParam);
+			}
+			break;
+		case WM_PAINT:
+			hdc = BeginPaint(hWnd, &ps);
+			// TODO:  在此添加任意绘图代码...
+			EndPaint(hWnd, &ps);
+			break;
+		case WM_DESTROY:
+			PostQuitMessage(0);
 			break;
 		default:
 			return DefWindowProc(hWnd, message, wParam, lParam);
-		}
-		break;
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-		// TODO:  在此添加任意绘图代码...
-		EndPaint(hWnd, &ps);
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
 	return 0;
 }
