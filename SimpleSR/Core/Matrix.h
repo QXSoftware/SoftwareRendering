6ee4dfc967514294394af9b2dd0bf6e2f9d58ccd
xiaocheng <lh563566994@126.com>
2015-04-22 12:56:17 +0800

    简化编写
    Signed-off-by: xiaocheng <lh563566994@126.com>

--- SimpleSR/Core/Matrix.h
+++ SimpleSR/Core/Matrix.h
@@ -1,27 +1,25 @@
 #ifndef Matrix_h__
 #define Matrix_h__
 
+#include "Vector.h"
+
 class Matrix4x4
 {
-	float m_Data[4][4];
+public:
+	float m00, m01, m02, m03;
+	float m10, m11, m12, m13;
+	float m20, m21, m22, m23;
+	float m30, m31, m32, m33;
 public:
 	Matrix4x4& operator=(const Matrix4x4&) = default;
 	Matrix4x4(const Matrix4x4&) = default;
-	Matrix4x4()
-	{
-		for (auto i = 0; i < 4; i++)
-		{
-			for (auto j = 0; j < 4; j++)
-			{
-				m_Data[i][j] = 0;
-			}
-		}
-	}
+	Matrix4x4() = default;
 	~Matrix4x4() = default;
 public:
-	float* operator[](int index)
+	Vector3 MultiplyVector(const Vector3& v)
 	{
-		return m_Data[index];
+		Vector3 x;
+		return x;
 	}
 };
 
