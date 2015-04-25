e4ba5c0b381e8e53e5b336b5bf4adccc0a44e6d0
xiaocheng <lh563566994@126.com>
2015-04-25 12:27:09 +0800

    添加方向光以及顶点数据类型
    Signed-off-by: xiaocheng <lh563566994@126.com>

---
+++ SimpleSR/Core/DirectionalLight.h
@@ -0,0 +1,15 @@
+#ifndef DirectionalLight_h__
+#define DirectionalLight_h__
+
+#include "Vector3.h"
+#include "Color.h"
+
+class DirectionalLight
+{
+public:
+    Vector3 Rotation;
+    float Intensity;
+    Color Col;
+};
+
+#endif // DirectionalLight_h__
---
+++ SimpleSR/Core/Vertex.h
@@ -0,0 +1,16 @@
+#ifndef Vertex_h__
+#define Vertex_h__
+
+#include "Vector2.h"
+#include "Vector3.h"
+#include "Vector4.h"
+
+struct Vertex
+{
+    Vector4 PosWorld;
+    Vector4 Pos;
+    Vector3 Normal;
+    Vector2 UV0;
+};
+
+#endif // Vertex_h__
